cf50b86f89c3ac8c9b19e8feb37f40abde45bba8
sgarmoe <110787643+sgarmoe@users.noreply.github.com>
2024-02-16 16:49:18 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -25,11 +25,11 @@ export const managers = [
       "roster": '732099569369899008',  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "name": "Brian Mcabee",
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
-      "location": "Rockville", // (optional)
+      "location": null, // (optional)
       "bio": "N/A.",
       "photo": "/squidward_bm", // square ratio recommended (no larger than 500x500)
-      "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
-      "favoriteTeam": "nyj", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "fantasyStart": null, // (optional) when did the manager start playing fantasy football
+      "favoriteTeam": null, // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: null, // Can be anything (usually your rival's name)
