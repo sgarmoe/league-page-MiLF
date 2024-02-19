9a3ac3e2754d3ea8cc40601abe78776e457567a0
sgarmoe <110787643+sgarmoe@users.noreply.github.com>
2024-02-19 18:01:40 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -3,7 +3,7 @@ export const leagueID = "1045634813593706496"; // your league ID
 export const leagueName = "Mildly intelligent Lovers of Football (MiLF)"; // your league name
 export const dues = 40; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
-export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
+export const enableBlog = true; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 // homepage info
 export const homepageText = `
